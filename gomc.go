d1921484ad2f11c8b0c325b4f65302f12c786897
takoyaki-3 <mail@takoyaki-3.com>
2020-12-29 15:42:29 +0900

    ZIPを追加

--- gomc.go
+++ gomc.go
@@ -1,16 +1,16 @@
 package gomc
 
 import (
-	"os"
-	"log"
-	"encoding/csv"
-	"io"
-	"bufio"
-	"io/ioutil"
-	"path/filepath"
-	"net/http"
 	"archive/zip"
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func newCsvReader(r io.Reader) *csv.Reader {
@@ -25,7 +25,7 @@ func newCsvReader(r io.Reader) *csv.Reader {
 	return csv.NewReader(br)
 }
 
-func ReadCSV(path string)(map[string]int,[][]string){
+func ReadCSV(path string) (map[string]int, [][]string) {
 	file, err := os.Open(path)
 	if err != nil {
 		panic(err)
@@ -36,8 +36,8 @@ func ReadCSV(path string)(map[string]int,[][]string){
 	reader.FieldsPerRecord = -1
 	var line []string
 
-	counter:=-1
-	titles :=map[string]int{}
+	counter := -1
+	titles := map[string]int{}
 	data := [][]string{}
 	for {
 		counter++
@@ -45,19 +45,19 @@ func ReadCSV(path string)(map[string]int,[][]string){
 		if err != nil {
 			break
 		}
-		if counter==0{
-			for k,v:=range line{
-				titles[v]=k
+		if counter == 0 {
+			for k, v := range line {
+				titles[v] = k
 			}
 			continue
 		}
-		data = append(data,line)
+		data = append(data, line)
 	}
 
-	return titles,data
+	return titles, data
 }
 
-func WriteCSV(path string,titles map[string]int,records [][]string){
+func WriteCSV(path string, titles map[string]int, records [][]string) {
 	file, err := os.Create(path)
 	if err != nil {
 		log.Println(err)
@@ -67,8 +67,8 @@ func WriteCSV(path string,titles map[string]int,records [][]string){
 	writer := csv.NewWriter(file) // utf8
 
 	header := []string{}
-	for k,v:=range titles{
-		for v >= len(header){
+	for k, v := range titles {
+		for v >= len(header) {
 			header = append(header, "")
 		}
 		header[v] = k
@@ -76,7 +76,7 @@ func WriteCSV(path string,titles map[string]int,records [][]string){
 
 	writer.Write(header)
 
-	for _,line:=range records{
+	for _, line := range records {
 		writer.Write(line)
 	}
 
@@ -101,7 +101,7 @@ func Dirwalk(dir string) []string {
 	return paths
 }
 
-func DownloadURL(url string,path string)error{
+func DownloadURL(url string, path string) error {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	resp, err := client.Do(req)
@@ -112,8 +112,8 @@ func DownloadURL(url string,path string)error{
 
 	out, err := os.Create(path)
 	defer out.Close()
-	
-	_,err = io.Copy(out, resp.Body)
+
+	_, err = io.Copy(out, resp.Body)
 
 	return err
 }
@@ -137,15 +137,15 @@ func Unzip(src, dest string) error {
 			os.MkdirAll(path, f.Mode())
 		} else {
 			f, err := os.OpenFile(
-					path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+				path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
 			if err != nil {
-					return err
+				return err
 			}
 			defer f.Close()
 
 			_, err = io.Copy(f, rc)
 			if err != nil {
-					return err
+				return err
 			}
 		}
 	}
@@ -158,12 +158,12 @@ func Ziparchive(output string, paths []string) error {
 
 	//ZIPファイル作成
 	if compressedFile, err = os.Create(output); err != nil {
-			return err
+		return err
 	}
 	defer compressedFile.Close()
 
 	if err := compress(compressedFile, ".", paths); err != nil {
-			return err
+		return err
 	}
 
 	return nil
@@ -173,57 +173,57 @@ func compress(compressedFile io.Writer, targetDir string, files []string) error 
 	w := zip.NewWriter(compressedFile)
 
 	for _, filename := range files {
-			filepath := fmt.Sprintf("%s/%s", targetDir, "./onegtfs/" + filename)
-			info, err := os.Stat(filepath)
-			if err != nil {
-					return err
-			}
+		filepath := fmt.Sprintf("%s/%s", targetDir, "./onegtfs/"+filename)
+		info, err := os.Stat(filepath)
+		if err != nil {
+			return err
+		}
 
-			if info.IsDir() {
-					continue
-			}
+		if info.IsDir() {
+			continue
+		}
 
-			file, err := os.Open(filepath)
-			if err != nil {
-					return err
-			}
-			defer file.Close()
+		file, err := os.Open(filepath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
 
-			hdr, err := zip.FileInfoHeader(info)
-			if err != nil {
-					return err
-			}
+		hdr, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
 
-			hdr.Name = filename
+		hdr.Name = filename
 
-			f, err := w.CreateHeader(hdr)
-			if err != nil {
-					return err
-			}
+		f, err := w.CreateHeader(hdr)
+		if err != nil {
+			return err
+		}
 
-			contents, _ := ioutil.ReadFile(filepath)
-			_, err = f.Write(contents)
-			if err != nil {
-					return err
-			}
+		contents, _ := ioutil.ReadFile(filepath)
+		_, err = f.Write(contents)
+		if err != nil {
+			return err
+		}
 	}
 
 	if err := w.Close(); err != nil {
-			return err
+		return err
 	}
 	return nil
 }
 
-func DirwalkPF(dir string) ([]string,[]string) {
+func DirwalkPF(dir string) ([]string, []string) {
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(err)
 	}
 
-	var paths,file_names []string
+	var paths, file_names []string
 	for _, file := range files {
 		paths = append(paths, filepath.Join(dir, file.Name()))
-		file_names = append(file_names,file.Name())
+		file_names = append(file_names, file.Name())
 	}
-	return paths,file_names
+	return paths, file_names
 }
