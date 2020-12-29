8eb9bd53ba20838433055de1639438660fd3b0c1
takoyaki-3 <mail@takoyaki-3.com>
2020-12-29 15:39:03 +0900

    Zip系を追加

--- gomc.go
+++ gomc.go
@@ -9,6 +9,8 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"net/http"
+	"archive/zip"
+	"fmt"
 )
 
 func newCsvReader(r io.Reader) *csv.Reader {
@@ -114,4 +116,114 @@ func DownloadURL(url string,path string)error{
 	_,err = io.Copy(out, resp.Body)
 
 	return err
+}
+
+func Unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		path := filepath.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			f, err := os.OpenFile(
+					path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+					return err
+			}
+			defer f.Close()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+					return err
+			}
+		}
+	}
+	return nil
+}
+
+func Ziparchive(output string, paths []string) error {
+	var compressedFile *os.File
+	var err error
+
+	//ZIPファイル作成
+	if compressedFile, err = os.Create(output); err != nil {
+			return err
+	}
+	defer compressedFile.Close()
+
+	if err := compress(compressedFile, ".", paths); err != nil {
+			return err
+	}
+
+	return nil
+}
+
+func compress(compressedFile io.Writer, targetDir string, files []string) error {
+	w := zip.NewWriter(compressedFile)
+
+	for _, filename := range files {
+			filepath := fmt.Sprintf("%s/%s", targetDir, "./onegtfs/" + filename)
+			info, err := os.Stat(filepath)
+			if err != nil {
+					return err
+			}
+
+			if info.IsDir() {
+					continue
+			}
+
+			file, err := os.Open(filepath)
+			if err != nil {
+					return err
+			}
+			defer file.Close()
+
+			hdr, err := zip.FileInfoHeader(info)
+			if err != nil {
+					return err
+			}
+
+			hdr.Name = filename
+
+			f, err := w.CreateHeader(hdr)
+			if err != nil {
+					return err
+			}
+
+			contents, _ := ioutil.ReadFile(filepath)
+			_, err = f.Write(contents)
+			if err != nil {
+					return err
+			}
+	}
+
+	if err := w.Close(); err != nil {
+			return err
+	}
+	return nil
+}
+
+func DirwalkPF(dir string) ([]string,[]string) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths,file_names []string
+	for _, file := range files {
+		paths = append(paths, filepath.Join(dir, file.Name()))
+		file_names = append(file_names,file.Name())
+	}
+	return paths,file_names
 }
